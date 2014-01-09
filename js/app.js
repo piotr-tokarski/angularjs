cbf3cae5d1f7c252d521948823f4356e47106902
tokarp <piotr.tokarski@stepstone.com>
2014-01-09 17:48:29 +0100

    [url params] display two airports at once
    *  using rout parameters as key for map with airport definition

--- js/app.js
+++ js/app.js
@@ -5,6 +5,12 @@ function airlineRouter ($routeProvider) {
 	$routeProvider
 		.when('/', {templateUrl: 'partials/destinations.html',
 		 controller: 'DestinationsCtrl'})
+        .when('airports/:airportCode', {templateUrl: 'partials/airports.html',
+            controller: 'AirportCtrl'
+        })
+        .when('/airports/:airport1/:airport2', {
+            templateUrl: 'partials/two_airports.html'
+        })
 		.when('/flights', {template: '<h3>Flights</h3> {{ airports | json }}',
 		 controller: 'FlightsCtrl'})
 		.when('/reservations', {template: '<h3>Your Reservations</h3> {{ airports | json }}',
--- js/controllers/app.js
+++ js/controllers/app.js
@@ -1,11 +1,13 @@
 function AppCtrl ($scope) {
+  $scope.airportTemplate = 'partials/airport.html';  
+
   $scope.setActive = function (type) {
     $scope.destinationsActive = '';
     $scope.flightsActive = '';
     $scope.reservationsActive = '';
 
     $scope[type + 'Active'] = 'active';
-  }
+  };
 
   $scope.airports = {
     "PDX": {
